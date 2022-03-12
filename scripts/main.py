1bf55d20a938993b22d67f5afcfeb3e7f289427c
Fedor Kondratenko <dostoyewski@yandex.ru>
2022-03-12 13:32:28 +0300

    Arch for net4

--- scripts/main.py
+++ scripts/main.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
     os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
     # Для Net4: 0.8986447222222222 решенных
     # Для Net2: 0.6945472222222222 решенных
-    drl = DeepRL(network=Net)
+    drl = DeepRL(network=Net4)
     drl.train()
     fig, ax = plt.subplots(nrows=1, ncols=1)
     ax.plot(drl.rewards)
@@ -110,4 +110,4 @@ if __name__ == '__main__':
             p += 1
     print(m / (m + p))
     make_gif_animation('frames')
-    drl.save('../models/net.json')
+    drl.save('../models/net4.json')
